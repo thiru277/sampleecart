0cf95242d6b46d15c0223a53fb66881231bc7835
thiru277 <thiruvambighai.s@gmail.com>
2022-03-02 22:11:03 +0530

    some code added

---
+++ src/HelloSample.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class HelloSample {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		
+		
+		System.out.println("enter name");
+	   String name =s.nextLine();
+	   System.out.println(name);
+		System.out.println("Enter your age");
+		 s.nextByte();
+
+	}
+}
